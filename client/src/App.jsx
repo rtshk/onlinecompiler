6f2ece12d325ef73c77c5c84e78ac6faea62eadc
rtshk <riteshnzee@gmail.com>
2025-01-26 00:16:45 +0530

    removing auth temperorily

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import Home from "./Components/Home";
-import Signup from "./Components/Signup";
-import Login from "./Components/Login";
 import Notes from "./Components/Notes";
-import ChatGPT from "./Components/ChatGPT";
+import Gemini from "./Components/Gemini";
 import InputOutput from "./Components/InputOutput";
 import Complexity from "./Components/Complexity";
 
@@ -15,13 +13,10 @@ function App() {
         {/* Home Route */}
         <Route path="/" element={<Home />}>
           <Route path="notes" element={<Notes />} />
-          <Route path="chatbot" element = {<ChatGPT/>}/>
+          <Route path="chatbot" element = {<Gemini/>}/>
           <Route path="/" element = {<InputOutput/>}/>
           <Route path="/complexity" element = {<Complexity/>}/>
         </Route>
-        {/* Auth Routes with Layout */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
