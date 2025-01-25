e646e79129a64451a83c53e5dad9f9ad607556bd
rtshk <riteshnzee@gmail.com>
2025-01-26 00:17:11 +0530

    adding basic code compiling functionality

--- client/src/Components/CodeEditor.jsx
+++ client/src/Components/CodeEditor.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
-import { useDispatch } from "react-redux";
-import { setCode } from "../redux/mainSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setCode, setOutput } from "../redux/mainSlice";
+import axios from "axios";
 
 const CodeEditor = () => {
   const dispatch = useDispatch();
+  const main = useSelector(state => state.main)
+  const code = main.code;
+  const input = main.input;
+  const handleCompile = async () => {
+    try {
+      const result = await axios.post(`http://localhost:8000/compile/`, {code, input})
+      console.log(result)
+      dispatch(setOutput(result.data.output));
+    } catch (error) {
+     console.log(error) 
+    }
+  }
 
   return (
     <div className="p-2 pt-0 pr-0 bg-[#1e1e1e] rounded-md">
       <div className="flex justify-end pb-1 text-white ">
-        <button className="p-1 px-6 rounded-tr-md rounded-bl-md bg-[#1DB954] hover:bg-[#159943]">
+        <button className="p-1 px-6 rounded-tr-md rounded-bl-md bg-[#1DB954] hover:bg-[#159943]"
+        onClick={handleCompile}>
           Run
         </button>
       </div>
@@ -17,61 +31,9 @@ const CodeEditor = () => {
         theme="vs-dark"
         defaultLanguage="cpp"
         defaultValue={`#include <iostream>
-#include <vector>
-
 using namespace std;
-
-vector<int> merge_sort(vector<int> arr) {
-    if (arr.size() <= 1) {
-        return arr;
-    }
-
-    int mid = arr.size() / 2;
-    vector<int> left_half = merge_sort(vector<int>(arr.begin(), arr.begin() + mid));
-    vector<int> right_half = merge_sort(vector<int>(arr.begin() + mid, arr.end()));
-
-    return merge(left_half, right_half);
-}
-
-vector<int> merge(vector<int> left, vector<int> right) {
-    vector<int> merged;
-    int left_idx = 0;
-    int right_idx = 0;
-
-    while (left_idx < left.size() && right_idx < right.size()) {
-        if (left[left_idx] <= right[right_idx]) {
-            merged.push_back(left[left_idx]);
-            left_idx++;
-        } else {
-            merged.push_back(right[right_idx]);
-            right_idx++;
-        }
-    }
-
-    while (left_idx < left.size()) {
-        merged.push_back(left[left_idx]);
-        left_idx++;
-    }
-
-    while (right_idx < right.size()) {
-        merged.push_back(right[right_idx]);
-        right_idx++;
-    }
-
-    return merged;
-}
-
 int main() {
-    vector<int> unsorted_list = {38, 27, 43, 3, 9, 82, 10};
-    vector<int> sorted_list = merge_sort(unsorted_list);
-
-    cout << "Sorted list: ";
-    for (int num : sorted_list) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
+   
 }`}
         height="83.8vh"
         onChange={(value) => {
